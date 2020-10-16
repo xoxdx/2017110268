bc6469d9ba76074ac15bcd7f77de74985d995b48
Kim Dasol <soldaxx@khu.ac.kr>
2020-10-16 10:46:16 +0900

    file updated

---
+++ MultiplicationTable.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	for (i = 1; i <= 9; i++)
+	{
+		for (j = 1; j < 9; j++)
+		{
+			cout << i << " X " << j << " = " << i * j << endl;
+		}
+		cout << endl;
+	}
+
+	return;
+}
